a13ed904fce63f3222a712156f645e44517de3f8
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-07-05 22:22:12 -0400

    starting employee tests

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,2 +1,8 @@
 const Employee = require("../lib/Employee");
 
+describe("Employee", () => {
+    it("Should return an object", () => {
+        const newEmployee = new Employee();
+        expect(typeof newEmployee).toBe("object");
+    });
+})
