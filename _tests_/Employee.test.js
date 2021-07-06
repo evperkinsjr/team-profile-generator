02f553b09ec56672d9de84b0fd45181713618eae
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-07-05 22:49:43 -0400

    still writing employee tests

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -3,6 +3,13 @@ const Employee = require("../lib/Employee");
 describe("Employee", () => {
     it("Should return an object", () => {
         const newEmployee = new Employee();
-        expect(typeof newEmployee).toBe("object");
+        expect(typeof(newEmployee)).toBe("object");
+    });
+
+    it("Should create an object with specific properties", () => {
+        const newEmployee = new Employee('Enrico', 24, 'enrico@test.com');
+        expect(newEmployee.name).toEqual('Enrico');
+        expect(newEmployee.id).toEqual(24);
+        expect(newEmployee.email).toEqual('enrico@test.com');
     });
 })
