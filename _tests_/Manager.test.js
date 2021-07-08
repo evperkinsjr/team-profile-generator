353a9e710b8a1673f04c14291744c1a384f7c796
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-07-08 15:07:10 -0400

    wrote tests for the Manager.js file

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,2 +1,34 @@
+const Employee = require("../lib/Employee");
 const Manager = require("../lib/Manager");
 
+describe("Manager", () => {
+    it("Should return an object", () => {
+        const newManager = new Manager();
+        expect(typeof(newManager)).toBe("object");
+    });
+
+    it("Should return an object with specific properties", () => {
+        const newManager = new Manager('Enrico', 24, 'enrico@test.com', '123-456-7890');
+        expect(newManager.name).toEqual('Enrico');
+        expect(newManager.id).toEqual(24);
+        expect(newManager.email).toEqual('enrico@test.com');
+        expect(newManager.officeNumber).toEqual('123-456-7890');
+    });
+
+    describe("getOfficeNumber", () => {
+        it("Should return a string", () => {
+            const newManager = new Manager('Enrico', 24, 'enrico@test.com', '123-456-7890');
+            const returnOfficeNumber = newManager.officeNumber;
+            expect(typeof(returnOfficeNumber)).toBe("string");
+        });
+    });
+
+    describe("getRole", () => {
+        it("Should return Manager as the role", () => {
+            const newManager = new Manager();
+            const returnRole = newManager.getRole();
+            expect(returnRole).toEqual("Manager");
+        });
+    });
+    
+});
