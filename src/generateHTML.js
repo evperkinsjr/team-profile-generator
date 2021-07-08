be430524d9c194e8983da7d876944191998deff6
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-07-08 18:16:12 -0400

    added function to create the html document; exported the document and employee card html

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -62,3 +62,33 @@ function devTeamHTML(team) {
         }
     }
 }
+
+function generateHTML(team) {
+    devTeamHTML(team);
+
+    return `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"/>
+        <title>My Team</title>
+      </head>
+      <body>
+        <div class="jumbotron-fluid bg-danger text-white">
+          <div class="container p-2">
+            <h1 class="display-2 fw-bold">My Team</h1>
+          </div>
+        </div>
+        <div class="row row-cols-4 g-1 pt-3 ms-5 me-5 justify-content-center">
+        ${employeeCards.join("")}
+      </div>
+      </body>
+    </html>`;
+}
+
+module.exports = {
+    generateHTML,
+    devTeamHTML,
+};
