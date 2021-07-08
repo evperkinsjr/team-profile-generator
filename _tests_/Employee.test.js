37eca5ce8fdf11fd52815a5caec6b970c96c913f
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-07-08 11:46:56 -0400

    wrote tests for functions in the Employee.js file

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -12,4 +12,36 @@ describe("Employee", () => {
         expect(newEmployee.id).toEqual(24);
         expect(newEmployee.email).toEqual('enrico@test.com');
     });
-})
+
+    describe("getName", () => {
+        it("Should return a string", () => {
+            const newEmployee = new Employee('Enrico', 24, 'enrico@test.com');
+            const returnName = newEmployee.getName();
+            expect(typeof(returnName)).toBe("string");
+        });
+    });
+
+    describe("getId", () => {
+        it("Should return a string", () => {
+            const newEmployee = new Employee('Enrico', 24, 'enrico@test.com');
+            const returnId = newEmployee.getId();
+            expect(typeof(returnId)).toBe("number");
+        });
+    });
+
+    describe("getEmail", () => {
+        it("Should return a string", () => {
+            const newEmployee = new Employee('Enrico', 24, 'enrico@test.com');
+            const returnEmail = newEmployee.getEmail();
+            expect(typeof(returnEmail)).toBe("string");
+        });
+    });
+
+    describe("getRole", () => {
+        it("Should return Employee as the role", () => {
+            const newEmployee = new Employee('Enrico', 24, 'enrico@test.com');
+            const returnRole = newEmployee.getRole();
+            expect(returnRole).toEqual("Employee");
+        });
+    });
+});
