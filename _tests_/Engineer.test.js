1d2131226aed17ab13a2bc0f0aaa29cdcc058260
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-07-08 12:23:32 -0400

    starting Engineer tests

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,3 +1,19 @@
 const Employee = require("../lib/Employee");
 const Engineer = require("../lib/Engineer");
 
+describe("Engineer", () => {
+    it("Should return an object", () => {
+        const newEngineer = new Engineer();
+        expect(typeof(newEngineer)).toBe("object");
+    });
+
+    it("Should create an object with specific properties", () => {
+        const newEngineer = new Engineer('Enrico', 24, 'enrico@test.com', 'evperkinsjr');
+        expect(newEngineer.name).toEqual('Enrico');
+        expect(newEngineer.id).toEqual(24);
+        expect(newEngineer.email).toEqual('enrico@test.com');
+        expect(newEngineer.gitHub).toEqual('evperkinsjr');
+    });
+
+    
+})
